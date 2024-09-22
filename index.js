20742b5b8ea277b15120d21917c18ab9a23f35ea
Carl <trusteddanger160@gmail.com>
2024-09-22 07:40:00 +0300

    RELEASE 2024

---
+++ index.js
@@ -0,0 +1,167 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
+const PORT = 8080;
+
+// Function to generate a unique user ID based on user-agent and IP
+const generateUserId = (req) => {
+    return crypto.createHash('sha256').update(req.headers['user-agent'] + req.ip).digest('hex');
+};
+
+// Function to check if a user is banned
+const checkIfBanned = (userId) => {
+    const bannedUsers = process.env.BANNED_USERS ? process.env.BANNED_USERS.split('/') : [];
+    console.log('Banned Users:', bannedUsers); // Debugging
+    console.log('User ID:', userId); // Debugging
+    return bannedUsers.includes(userId);
+};
+
+const generateNonce = () => crypto.randomBytes(16).toString('base64');
+
+app.use((req, res, next) => {
+    res.locals.nonce = generateNonce();
+    next();
+});
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'none'"],
+            imgSrc: ["'self'", "https://gido-web.onrender.com", "data:", "https://cdn.discordapp.com", "https://images-ext-1.discordapp.net", "https://media.discordapp.net/attachments/", "https://media.discordapp.net/", "/images/"],
+            connectSrc: ["'self'", "https://gido-web.onrender.com", "https://fetch-bot-1.onrender.com"],
+            scriptSrc: ["'self'", "https://gido-web.onrender.com"],
+            styleSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`, "https://gido-web.onrender.com"],
+        },
+    },
+}));
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Security-Policy', `style-src 'self' 'nonce-${res.locals.nonce}'`);
+    const imagePath = path.join(__dirname, 'main-web/index.html');
+    res.sendFile(imagePath);
+});
+
+app.get('/404.css', (req, res) => {
+    const imagePath = path.join(__dirname, 'main-web/errors/404/404.css');
+    res.sendFile(imagePath);
+});
+
+app.get('/styles.css', (req, res) => {
+    const imagePath = path.join(__dirname, 'main-web/styles.css');
+    res.sendFile(imagePath);
+});
+
+app.get('/scripts.js', (req, res) => {
+    const imagePath = path.join(__dirname, 'main-web/scripts.js');
+    res.setHeader('Content-Type', 'application/javascript');
+    res.sendFile(imagePath);
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/support', async (req, res) => {
+    const { nickname, email, description } = req.body;
+    const webhookURL = process.env.WEBHOOK_URL;
+    const userId = generateUserId(req);
+
+    // Check if nickname, email, or description is missing
+    if (!nickname || !email || !description) {
+        return res.status(400).json({ message: 'All fields are required.' });
+    }
+
+    // Check if user is banned
+    if (checkIfBanned(userId)) {
+        return res.status(403).json({
+            message: 'You are banned from submitting requests. Please contact support on Discord for more info: https://discord.gg/Gq48UpPrXH',
+        });
+    }
+
+    // Extract IP address
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    const embed = {
+        content: `<@&1272577631191175272>`,
+        embeds: [
+            {
+                title: "Support Request",
+                color: 0x3498db,
+                fields: [
+                    {
+                        name: "Nickname",
+                        value: nickname,
+                        inline: false
+                    },
+                    {
+                        name: "Email",
+                        value: email,
+                        inline: false
+                    },
+                    {
+                        name: "Description",
+                        value: description,
+                        inline: false
+                    },
+                    {
+                        name: "User ID",
+                        value: userId,
+                        inline: false
+                    },
+                    {
+                        name: "IP Address",
+                        value: ipAddress,
+                        inline: false
+                    }
+                ],
+                footer: {
+                    text: "Help Within 24 Hours!",
+                },
+                timestamp: new Date().toISOString()
+            }
+        ]
+    };
+
+    try {
+        const response = await axios.post(webhookURL, embed);
+
+        if (response.status === 204) {
+            res.status(200).json({ message: 'Request submitted successfully. You will receive an email within 24 hours.' });
+        } else {
+            console.error('Failed to submit request:', response.statusText);
+            res.status(500).json({ message: 'Failed to submit request.' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Error submitting request.' });
+    }
+});
+
+process.on('uncaughtException', (err) => {
+    console.log(`Unexpected error: ${err.message}`, err);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled promise rejection:', reason);
+    process.exit(1);
+});
+
+app.get('/images/*', (req, res) => {
+    const imageName = req.params[0];
+    const imagePath = path.join(__dirname, 'main-web/images', imageName);
+    res.sendFile(imagePath);
+});
+
+app.use((req, res, next) => {
+    if (req.accepts('html')) {
+        res.status(404).sendFile(path.join(__dirname, 'main-web/errors/404/404.html'));
+    } else {
+        res.status(404).send('Resource not found');
+    }
+});
+
+app.listen(PORT, () => console.log(`Online on: ${PORT}`));
---
+++ main-web/scripts.js
@@ -0,0 +1,188 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const header = document.querySelector('header');
+    const body = document.body;
+    const supportForm = document.getElementById('support-form');
+    const statusIndicator = document.getElementById('status-indicator');
+    const errorMessage = document.getElementById('error-message');
+    const notificationContainer = document.createElement('div');
+    let lastScrollTop = 0;
+
+    const handleScrollAnimation = () => {
+        document.querySelectorAll('.scroll-section').forEach(section => {
+            const rect = section.getBoundingClientRect();
+            section.classList.toggle('visible', rect.top < window.innerHeight && rect.bottom > 0);
+        });
+    };
+
+    function setActiveLink(targetId) {
+        document.querySelectorAll('nav a').forEach(link => {
+            link.classList.remove('active', 'highlight');
+            if (link.getAttribute('href') === `#${targetId}`) {
+                link.classList.add('active', 'highlight');
+                setTimeout(() => link.classList.remove('highlight'), 3000);
+            }
+        });
+    }
+
+        function scrollToSection(element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    }
+        const firstSection = document.querySelector('section');
+    if (firstSection) {
+        scrollToSection(firstSection);
+    }
+
+    window.addEventListener('scroll', () => {
+        handleScrollAnimation();
+
+        let current = '';
+        document.querySelectorAll('section').forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+
+            if (scrollY >= (sectionTop - 50) && scrollY < (sectionTop + sectionHeight - 50)) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        setActiveLink(current);
+
+        const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        header.classList.toggle('hidden', currentScrollTop > lastScrollTop);
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+    });
+
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            scrollToSection(targetElement);
+            setActiveLink(targetId);
+        });
+    });
+
+    handleScrollAnimation();
+
+    const toggleButton = document.createElement('button');
+    toggleButton.classList.add('header-toggle');
+    toggleButton.innerHTML = '&#9660;';
+    document.body.appendChild(toggleButton);
+
+    toggleButton.addEventListener('click', () => {
+        header.classList.toggle('hidden');
+        toggleButton.innerHTML = header.classList.contains('hidden') ? '&#9650;' : '&#9660;';
+    });
+
+    const darkModeKey = 'gidoBotDarkMode';
+    if (localStorage.getItem(darkModeKey) === 'enabled') {
+        body.classList.add('dark-mode');
+        themeToggle.textContent = '☀️';
+    } else {
+        themeToggle.textContent = '🌙';
+    }
+
+    themeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        const isDarkMode = body.classList.contains('dark-mode');
+        localStorage.setItem(darkModeKey, isDarkMode ? 'enabled' : '');
+        themeToggle.textContent = isDarkMode ? '☀️' : '🌙';
+    });
+
+    if (supportForm) {
+        supportForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const now = new Date().getTime();
+            const tenHours = 10 * 60 * 60 * 1000; // 10 hours
+            const lastReportTime = parseInt(localStorage.getItem('lastReportTime'), 10) || 0;
+            const reportCount = parseInt(localStorage.getItem('reportCount'), 10) || 0;
+
+            if (now - lastReportTime < tenHours) {
+                if (reportCount >= 2) {
+                    const remainingTime = tenHours - (now - lastReportTime);
+                    const hours = Math.floor(remainingTime / (60 * 60 * 1000));
+                    const minutes = Math.floor((remainingTime % (60 * 60 * 1000)) / (60 * 1000));
+                    alert(`You can only report twice every 10 hours. Please try again in ${hours} hour(s) and ${minutes} minute(s).`);
+                    return;
+                } else {
+                    localStorage.setItem('reportCount', reportCount + 1);
+                }
+            } else {
+                localStorage.setItem('reportCount', 1);
+                localStorage.setItem('lastReportTime', now);
+            }
+
+            const formData = new FormData(supportForm);
+            const email = formData.get('user-email');
+            const description = formData.get('user-description');
+
+            try {
+                const response = await fetch('https://gido-bot-web.onrender.com/api/support', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email, description })
+                });
+
+                if (response.ok) {
+                    alert('Report submitted successfully.');
+                    localStorage.setItem('lastReportTime', now.toString());
+                    supportForm.reset();
+                } else {
+                    alert('Failed to submit report. Please try again later.');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                alert('Failed to submit report. Please try again later.');
+            }
+        });
+    }
+
+
+    const showNotification = (message, type) => {
+        notificationContainer.className = `notification ${type}`;
+        notificationContainer.textContent = message;
+        document.body.appendChild(notificationContainer);
+        setTimeout(() => notificationContainer.remove(), 5000);
+    };
+
+    const checkStatus = async () => {
+        const statusIndicator = document.getElementById('status-indicator');
+        const errorMessage = document.getElementById('error-message');
+        
+        try {
+            const response = await fetch('https://fetch-bot-1.onrender.com/api/status');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            if (data.status === 'online') {
+                statusIndicator.classList.add('online');
+                statusIndicator.style.backgroundColor = '#4CAF50';
+                statusIndicator.textContent = 'Online';
+            } else {
+                statusIndicator.classList.remove('online');
+                statusIndicator.style.backgroundColor = '#FF0000'; 
+                statusIndicator.textContent = 'Offline';
+            }
+        } catch (error) {
+            console.error('Failed to fetch status:', error);
+            statusIndicator.style.backgroundColor = '#FF0000';
+            statusIndicator.textContent = 'Error fetching status';
+            errorMessage.textContent = 'Unable to check status. Please try again later.';
+            const someButton = document.getElementById('someButton');
+            const additionalContent = document.getElementById('additionalContent');
+            if (someButton) someButton.disabled = true;
+            if (additionalContent) additionalContent.style.display = 'none';
+        }
+    };
+
+    window.addEventListener('online', checkStatus);
+    window.addEventListener('offline', checkStatus);
+    checkStatus();
+});
