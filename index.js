61197691a5442e65b7cc2fb0b7000199c73ac5fe
Carl <trusteddanger160@gmail.com>
2024-09-30 22:58:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,13 +52,37 @@ app.use((req, res, next) => {
 
 app.use(helmet({
     contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'none'"],
-            imgSrc: ["'self'", "https://gido-web.ooguy.com", "data:", "https://cdn.discordapp.com", "https://images-ext-1.discordapp.net", "https://media.discordapp.net/attachments/", "https://media.discordapp.net/", "/images/"],
-            connectSrc: ["'self'", "https://gido-web.ooguy.com", "https://fetch-bot-fvty.onrender.com/", "https://asdfasdfasfasdfdasfwdasdi.onrender.com"],
-            scriptSrc: ["'self'", "https://gido-web.ooguy.com", "https://asdfasdfasfasdfdasfwdasdi.onrender.com"],
-            styleSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`, "https://gido-web.ooguy.com", "https://asdfasdfasfasdfdasfwdasdi.onrender.com"],
-        },
+       directives: {
+  defaultSrc: ["'none'"],
+  imgSrc: [
+    "'self'",
+    "https://gido-web.ooguy.com",
+    "data:",
+    "https://cdn.discordapp.com",
+    "https://images-ext-1.discordapp.net",
+    "https://media.discordapp.net/attachments/",
+    "https://media.discordapp.net/",
+    "/images/"
+  ],
+  connectSrc: [
+    "'self'",
+    "https://gido-web.ooguy.com",
+    "https://fetch-bot-fvty.onrender.com",
+    "https://asdfasdfasfasdfdasfwdasdi.onrender.com"
+  ],
+  scriptSrc: [
+    "'self'",
+    "https://gido-web.ooguy.com",
+    "https://asdfasdfasfasdfdasfwdasdi.onrender.com"
+  ],
+  styleSrc: [
+    "'self'",
+    (req, res) => `'nonce-${res.locals.nonce}'`,
+    "https://gido-web.ooguy.com",
+    "https://asdfasdfasfasdfdasfwdasdi.onrender.com"
+  ]
+},
+
     },
 }));
 
